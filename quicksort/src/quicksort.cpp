77113bb025f9d5a5ba87fc7dc77013efb3661579
johnbanq <cardinalist@yeah.net>
2020-08-30 15:58:22 +0800

    refactor: modify signature

--- quicksort/src/quicksort.cpp
+++ quicksort/src/quicksort.cpp
@@ -1,14 +1,20 @@
 #include <algorithm>
 #include "tgi/quicksort/quicksort.h"
 
-size_t quicksort_partition(std::vector<int>& array, size_t from, size_t to) {
-    std::sort(array.begin()+from, array.begin()+to);
-    return ((to - from) / 2) + from;
+struct vector_view {
+    std::vector<int>& array;
+    const size_t from;
+    const size_t to;
+};
+
+size_t quicksort_partition(vector_view v) {
+    std::sort(v.array.begin()+v.from, v.array.begin()+v.to);
+    return ((v.to - v.from) / 2) + v.from;
 }
 
 void quicksort_recursive(std::vector<int>& array, size_t from, size_t to) {
     if(from<to) {
-        auto pivot = quicksort_partition(array, from, to);
+        auto pivot = quicksort_partition({ array, from, to});
         quicksort_recursive(array, from, pivot);
         quicksort_recursive(array, pivot + 1, to);
     }
