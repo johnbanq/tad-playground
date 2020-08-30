669c98062d7f1c68e16cc3e72283ac23eefaa268
johnbanq <cardinalist@yeah.net>
2020-08-30 15:44:45 +0800

    feat(quicksort): oops, forgot the +1

--- quicksort/src/quicksort.cpp
+++ quicksort/src/quicksort.cpp
@@ -1,5 +1,20 @@
+#include <algorithm>
 #include "tgi/quicksort/quicksort.h"
 
+size_t quicksort_partition(std::vector<int>& array, size_t from, size_t to) {
+    std::sort(array.begin()+from, array.begin()+to);
+    return (to - from) / 2;
+}
+
+void quicksort_recursive(std::vector<int>& array, size_t from, size_t to) {
+    if(from<to) {
+        auto pivot = quicksort_partition(array, from, to);
+        quicksort_recursive(array, from, pivot);
+        quicksort_recursive(array, pivot, to);
+    }
+}
+
 std::vector<int> quicksort(std::vector<int> array) {
-    return {};
+    quicksort_recursive(array, 0, array.size());
+    return array;
 }
--- quicksort/test/test_quicksort.cpp
+++ quicksort/test/test_quicksort.cpp
@@ -4,6 +4,6 @@
 
 #include "tgi/quicksort/quicksort.h"
 
-TEST_CASE( "quicksort works", "[quicksort]" ) {
+TEST_CASE( "quicksort works on 3", "[quicksort]" ) {
     REQUIRE(quicksort(std::vector<int>{3,2,1}) == std::vector<int>{1, 2, 3});
 }
