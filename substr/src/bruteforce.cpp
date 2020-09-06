316c61d81cb880a6cc2c8a66fbfbf8f549178074
johnbanq <cardinalist@yeah.net>
2020-09-06 17:44:24 +0800

    refactor(substr): cleanup bruteforce

--- substr/src/bruteforce.cpp
+++ substr/src/bruteforce.cpp
@@ -1,5 +1,5 @@
 #include "tgi/substr/bruteforce.h"
-#include <iostream>
+
 int substr_bruteforce(std::string_view source, std::string_view pattern) {
     for (auto i = 0; i <= (int)source.size() - (int)pattern.size(); i++) {
         if(std::equal(source.begin()+i, source.begin()+i+pattern.size(), pattern.begin())) {
