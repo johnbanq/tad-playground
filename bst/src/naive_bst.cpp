4d3c295d2177564884e1b04dd07f43e4a22eebb5
johnbanq <cardinalist@yeah.net>
2020-10-09 22:50:19 +0800

    realiszation: we screwed up

--- bst/src/naive_bst.cpp
+++ bst/src/naive_bst.cpp
@@ -14,13 +14,17 @@ node* parse(const std::string& literal, int offset) {
     if(literal == "null") {
         return nullptr;
     } else {
-        if(literal[0] == '(') {
+        if(literal[offset] == '(') {
             size_t parsed = 0;
             int value = std::stoi(literal.substr(1, literal.size()), &parsed, 10);
-            if(literal[0+parsed+1]==')') {
+            if(literal[offset+parsed+1]==')') {
                 return new node{value, nullptr, nullptr};
             } else {
-                throw std::invalid_argument{std::string{"unexpected charactor ["}+literal[0+parsed+1]+"] found at literal offset="+std::to_string(offset)};
+                if(literal[0+parsed+1]==':') {
+                    node* left = 
+                } else {
+                    throw std::invalid_argument{std::string{"unexpected charactor ["}+literal[0+parsed+1]+"] found at literal offset="+std::to_string(offset)};
+                }
             }
         } else {
             throw std::invalid_argument{std::string{"unexpected charactor ["}+literal[0]+"] found at literal offset="+std::to_string(offset)};
--- bst/test/test_naive_bst.cpp
+++ bst/test/test_naive_bst.cpp
@@ -17,3 +17,15 @@ TEST_CASE( "from_literal builds root tree", "[bst][literal]" ) {
     REQUIRE(tree.root->left == nullptr);
     REQUIRE(tree.root->right == nullptr);
 }
+
+TEST_CASE( "from_literal builds 1 child case", "[bst][literal]" ) {
+    auto tree = from_literal("(2:(1):null)");
+    REQUIRE(tree.root->value == 2);
+    REQUIRE(tree.root->left->value == 1);
+    REQUIRE(tree.root->right == nullptr);
+
+    tree = from_literal("(2:null:(3))");
+    REQUIRE(tree.root->value == 2);
+    REQUIRE(tree.root->left->value == 1);
+    REQUIRE(tree.root->right == nullptr);
+}
