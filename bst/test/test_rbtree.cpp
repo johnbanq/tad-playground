b843d5baee619f1ee7213a7b3e979c3e92b7c355
johnbanq <cardinalist@yeah.net>
2020-10-15 21:13:04 +0800

    test(bst/rbtree): add from_literal  test

---
+++ bst/test/test_rbtree.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+
+#include "catch.hpp"
+
+#include "test_bst_common.h"
+#include "tgi/bst/rbtree.h"
+#include "tgi/bst/internal_rbtree.h"
+
+using color = rbtree::color;
+
+TEST_CASE( "from_literal works on rbtree", "[rbtree][literal]" ) {
+    auto tree = rbtree_from_literal("(2:(1):(3))");
+    REQUIRE(tree.root->value == 2);
+    REQUIRE(tree.root->color == color::black);
+    REQUIRE(tree.root->left->parent == tree.root);
+    REQUIRE(tree.root->left->value == 1);
+    REQUIRE(tree.root->color == color::red);
+    REQUIRE(tree.root->right->parent == tree.root);
+    REQUIRE(tree.root->right->value == 3);
+    REQUIRE(tree.root->color == color::red);
+}
